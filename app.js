3b6913f2f1f85defa3bf4783659086a07e6c8d73
Tiana <tiana.musiyachenko@gmail.com>
2023-09-19 07:36:31 +0500

    added minutes input

--- app.js
+++ app.js
@@ -3,179 +3,222 @@ let timeHour = document.querySelector('.time-hour-text');
 let timeMinute = document.querySelector('.time-minute-text');
 let timeInform = document.querySelector('.time-info');
 
-let hour = +prompt('введите час');
-timeHour.textContent = hour;
-console.log(hour);
+//indexOf("a") - ф-ция поиска позиции элемета 'a'
+//slice(a,b) - ф-ция вырезает часть строки с позиции a, до позиции b
 
-switch (hour) {
-  case 0:
-    timeHour.textContent = '0';
-    timeInform.textContent = '0 часов ночи';
-    alert('0 часов ночи');
-    myImg.src = 'images/night.jpg';
-    break;
-  case 1:
-    alert('1 час ночи');
-    timeHour.textContent = '1';
-    timeInform.textContent = '1 час ночи';
-    myImg.src = 'images/night.jpg';
-    break;
-  case 2:
-    alert('2 часа ночи');
-    timeHour.textContent = '2';
-    timeInform.textContent = '2 часа ночи';
-    myImg.src = 'images/night.jpg';
-    console.log('timeInform');
-    break;
-  case 3:
-    alert('3 часа ночи');
-    timeHour.textContent = '3';
-    timeInform.textContent = '3 часа ночи';
-    console.log('timeInform');
-    myImg.src = 'images/night.jpg';
-    break;
-  case 4:
-    alert('4 часа ночи');
-    timeHour.textContent = '4';
-    timeInform.textContent = '4 часа ночи';
-    console.log('timeInform');
-    myImg.src = 'images/night.jpg';
-    break;
-  case 5:
-    alert('5 часов ночи');
-    timeHour.textContent = '5';
-    timeInform.textContent = '5 часов ночи';
-    console.log('timeInform');
-    myImg.src = 'images/night.jpg';
-    break;
-  case 6:
-    alert('6 часов ночи');
-    timeHour.textContent = '6';
-    timeInform.textContent = '6 часов ночи';
-    console.log('timeInform');
-    myImg.src = 'images/night.jpg';
-    break;
-  case 7:
-    alert('7 часов утра');
-    timeHour.textContent = '7';
-    timeInform.textContent = '7 часов утра';
-    console.log('timeInform');
-    myImg.src = 'images/morning.jpg';
-    break;
-  case 8:
-    alert('8 часов утра');
-    timeHour.textContent = '8';
-    timeInform.textContent = '8 часов утра';
-    console.log('timeInform');
-    myImg.src = 'images/morning.jpg';
-    break;
-  case 9:
-    alert('9 часов утра');
-    timeHour.textContent = '9';
-    timeInform.textContent = '9 часов утра';
-    console.log('timeInform');
-    myImg.src = 'images/morning.jpg';
-    break;
-  case 10:
-    alert('10 часов утра');
-    timeHour.textContent = '10';
-    timeInform.textContent = '10 часов утра';
-    console.log('timeInform');
-    myImg.src = 'images/morning.jpg';
-    break;
-  case 11:
-    alert('11 часов дня');
-    timeHour.textContent = '11';
-    timeInform.textContent = '11 часов дня';
-    console.log('timeInform');
-    myImg.src = 'images/day.jpg';
-    break;
-  case 12:
-    alert('12 часов дня');
-    timeHour.textContent = '12';
-    timeInform.textContent = '12 часов дня';
-    console.log('timeInform');
-    myImg.src = 'images/day.jpg';
-    break;
-  case 13:
-    alert('1 час дня');
-    timeHour.textContent = '1';
-    timeInform.textContent = '1 час дня';
-    console.log('timeInform');
-    myImg.src = 'images/day.jpg';
-    break;
-  case 14:
-    alert('2 часа дня');
-    timeHour.textContent = '2';
-    timeInform.textContent = '2 часа дня';
-    console.log('timeInform');
-    myImg.src = 'images/day.jpg';
-    break;
-  case 15:
-    alert('3 часа дня');
-    timeHour.textContent = '3';
-    timeInform.textContent = '3 часа дня';
-    console.log('timeInform');
-    myImg.src = 'images/day.jpg';
-    break;
-  case 16:
-    alert('4 часа дня');
-    timeHour.textContent = '4';
-    timeInform.textContent = '4 часа дня';
-    console.log('timeInform');
-    myImg.src = 'images/day.jpg';
-    break;
-  case 17:
-    alert('5 часов дня');
-    timeHour.textContent = '5';
-    timeInform.textContent = '5 часов дня';
-    console.log('timeInform');
-    myImg.src = 'images/day.jpg';
-    break;
-  case 18:
-    alert('6 часов вечера');
-    timeHour.textContent = '6';
-    timeInform.textContent = '6 часов вечера';
-    console.log('timeInform');
-    myImg.src = 'images/evening.jpg';
-    break;
-  case 19:
-    alert('7 часов вечера');
-    timeHour.textContent = '7';
-    timeInform.textContent = '7 часов вечера';
-    console.log('timeInform');
-    myImg.src = 'images/evening.jpg';
-    break;
-  case 20:
-    alert('8 часов вечера');
-    timeHour.textContent = '8';
-    timeInform.textContent = '8 часов вечера';
-    console.log('timeInform');
-    myImg.src = 'images/evening.jpg';
-    break;
-  case 21:
-    alert('9 часов вечера');
-    timeHour.textContent = '9';
-    timeInform.textContent = '9 часов вечера';
-    console.log('timeInform');
-    myImg.src = 'images/evening.jpg';
-    break;
-  case 22:
-    alert('10 часов вечера');
-    timeHour.textContent = '10';
-    timeInform.textContent = '10 часов вечера';
-    console.log('timeInform');
-    myImg.src = 'images/evening.jpg';
-    break;
-  case 23:
-    alert('11 часов вечера');
-    timeHour.textContent = '11';
-    timeInform.textContent = '11 часов вечера';
-    console.log('timeInform');
-    myImg.src = 'images/evening.jpg';
-    break;
+let testString = prompt('Enter the time in hh/mm format');
+// console.log(testString);
+let posSymbol = testString.indexOf('/');
+// console.log(posSymbol);
+let hour = testString.slice(0, posSymbol);
+// console.log(hour);
+let minute = testString.slice(posSymbol + 1);
+// console.log(minute);
 
-  default:
-    alert('Error');
-    break;
+if (hour > 23 || minute > 59) {
+  alert('Error');
+  // minute = '00';
+  // timeMinute.textContent = minute;
+} else {
+  // console.log('hour - ' + hour);
+  // console.log('minute - ' + minute);
+  // console.log(hour.slice(0, 1));
+  if (hour.slice(0, 1) == 0) {
+    hour = hour.slice(posSymbol - 1);
+    console.log('hour - ' + hour);
+    console.log('minute - ' + minute);
+    // minute = testString.slice(posSymbol + 1);
+    timeMinute.textContent = minute;
+    // console.log('timeMinute - ' + timeMinute);
+    // console.log('hour - ' + hour);
+  } else {
+    timeMinute.textContent = minute;
+    // hour = hour;
+    // timeHour.textContent = hour;
+
+    console.log('hour - ' + hour);
+    console.log('minute - ' + minute);
+    // console.log('hour - ' + hour);
+  }
+
+  // let hour = +prompt('введите час');
+  // timeHour.textContent = hour;
+  // console.log(timeHour.textContent);
+  // console.log(hour);
+
+  switch (+hour) {
+    case 0:
+      timeHour.textContent = '0';
+      timeInform.textContent = '0 часов ночи  ' + minute + ' минут';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      myImg.src = 'images/night.jpg';
+      break;
+    case 1:
+      timeHour.textContent = '1';
+      timeInform.textContent = '1 час ночи  ' + minute + ' минут';
+      myImg.src = 'images/night.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 2:
+      timeHour.textContent = '2';
+      timeInform.textContent = '2 часа ночи  ' + minute + ' минут';
+      myImg.src = 'images/night.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 3:
+      timeHour.textContent = '3';
+      timeInform.textContent = '3 часа ночи  ' + minute + ' минут';
+      myImg.src = 'images/night.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 4:
+      timeHour.textContent = '4';
+      timeInform.textContent = '4 часа ночи  ' + minute + ' минут';
+      myImg.src = 'images/night.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 5:
+      timeHour.textContent = '5';
+      timeInform.textContent = '5 часов ночи  ' + minute + ' минут';
+      myImg.src = 'images/night.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 6:
+      timeHour.textContent = '6';
+      timeInform.textContent = '6 часов ночи  ' + minute + ' минут';
+      myImg.src = 'images/night.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 7:
+      timeHour.textContent = '7';
+      timeInform.textContent = '7 часов утра  ' + minute + ' минут';
+      myImg.src = 'images/morning.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 8:
+      timeHour.textContent = '8';
+      timeInform.textContent = '8 часов утра  ' + minute + ' минут';
+      myImg.src = 'images/morning.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 9:
+      timeHour.textContent = '9';
+      timeInform.textContent = '9 часов утра  ' + minute + ' минут';
+      myImg.src = 'images/morning.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 10:
+      timeHour.textContent = '10';
+      timeInform.textContent = '10 часов утра  ' + minute + ' минут';
+      myImg.src = 'images/morning.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 11:
+      timeHour.textContent = '11';
+      timeInform.textContent = '11 часов дня  ' + minute + ' минут';
+      myImg.src = 'images/day.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 12:
+      timeHour.textContent = '12';
+      timeInform.textContent = '12 часов дня  ' + minute + ' минут';
+      myImg.src = 'images/day.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 13:
+      timeHour.textContent = '1';
+      timeInform.textContent = '1 час дня  ' + minute + ' минут';
+      myImg.src = 'images/day.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 14:
+      timeHour.textContent = '2';
+      timeInform.textContent = '2 часа дня  ' + minute + ' минут';
+      myImg.src = 'images/day.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 15:
+      timeHour.textContent = '3';
+      timeInform.textContent = '3 часа дня  ' + minute + ' минут';
+      myImg.src = 'images/day.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 16:
+      timeHour.textContent = '4';
+      timeInform.textContent = '4 часа дня  ' + minute + ' минут';
+      myImg.src = 'images/day.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 17:
+      timeHour.textContent = '5';
+      timeInform.textContent = '5 часов дня  ' + minute + ' минут';
+      myImg.src = 'images/day.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 18:
+      timeHour.textContent = '6';
+      timeInform.textContent = '6 часов вечера  ' + minute + ' минут';
+      myImg.src = 'images/evening.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 19:
+      timeHour.textContent = '7';
+      timeInform.textContent = '7 часов вечера  ' + minute + ' минут';
+      myImg.src = 'images/evening.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 20:
+      timeHour.textContent = '8';
+      timeInform.textContent = '8 часов вечера  ' + minute + ' минут';
+      myImg.src = 'images/evening.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 21:
+      timeHour.textContent = '9';
+      timeInform.textContent = '9 часов вечера  ' + minute + ' минут';
+      myImg.src = 'images/evening.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 22:
+      timeHour.textContent = '10';
+      timeInform.textContent = '10 часов вечера  ' + minute + ' минут';
+      myImg.src = 'images/evening.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+    case 23:
+      timeHour.textContent = '11';
+      timeInform.textContent = '11 часов вечера  ' + minute + ' минут';
+      myImg.src = 'images/evening.jpg';
+      console.log(timeInform.textContent);
+      alert(timeInform.textContent);
+      break;
+
+    default:
+      alert('Error');
+
+      break;
+  }
 }
